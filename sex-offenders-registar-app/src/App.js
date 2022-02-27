a7d8c35236b3081ecd170369b4eddef0724fd81a
RGibesh <ralphgibendi01@gmail.com>
2022-02-27 11:05:11 +0300

    created app

---
+++ sex-offenders-registar-app/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>You have reached the landing page of Sex Registar KE</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
