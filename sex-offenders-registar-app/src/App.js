805397db45ea3a6adcf8e27cc8bf4c6b221e73ba
Billy Gibendi <RGibesh@users.noreply.github.com>
2022-02-28 22:04:21 +0300

    Added paragraph

--- sex-offenders-registar-app/src/App.js
+++ sex-offenders-registar-app/src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>You have reached the landing page of Sex Registar KE</h1>
+    <p>This project aims to solve community issues with regards to sexual offences are concerned 
+    basing my use case as my residential country Kenya. Judicial proceeding need such a tool in the event of court proceedings<p>
       </header>
     </div>
   );
