7f6e4c7ccf8ee44f9911ef0ef1c8e69c3a48160a
RGibesh <ralphgibendi01@gmail.com>
2022-03-06 00:16:55 +0300

    edited app

--- sex-offenders-registar-app/src/App.js
+++ sex-offenders-registar-app/src/App.js
@@ -7,9 +7,10 @@ function App() {
       <header className="App-header">
         <h1>You have reached the landing page of Sex Registar KE</h1>
     <p>This project aims to solve community issues with regards to sexual offences are concerned 
-    basing my use case as my resident country is Kenya. Judicial proceeding need such a tool in the event of court proceedings
+    basing my use case as a resident country is Kenya. Judicial proceeding need such a tool in the event of court proceedings
     </p>
       </header>
+      <h5>Login here </h5>
     </div>
   );
 }
