bb1078a03674f42184966f187ed7898eafde4288
RGibesh <ralphgibendi01@gmail.com>
2022-03-06 00:01:47 +0300

    edited app paragraph syntax

--- sex-offenders-registar-app/src/App.js
+++ sex-offenders-registar-app/src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <h1>You have reached the landing page of Sex Registar KE</h1>
     <p>This project aims to solve community issues with regards to sexual offences are concerned 
-    basing my use case as my residential country Kenya. Judicial proceeding need such a tool in the event of court proceedings<p>
+    basing my use case as my residential country Kenya. Judicial proceeding need such a tool in the event of court proceedings
+    </p>
       </header>
     </div>
   );
