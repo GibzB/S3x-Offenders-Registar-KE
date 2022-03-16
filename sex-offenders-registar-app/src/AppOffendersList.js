6db8c1241999756b4b14ca1a06bb9c78e1ed08d6
RGibesh <ralphgibendi01@gmail.com>
2022-03-16 22:42:07 +0300

    created second page

---
+++ sex-offenders-registar-app/src/AppOffendersList.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import React, { useState, useEffect } from "react";
+
+function App() {
+  return (
+    <div className="App">
+  <h1 className="text-center mb-3">Characters</h1>
+  <div className="container">
+  <div className="row">
+    Filter component will be placed here
+    <div className="col-lg-8 col-12">
+      <div className="row">
+        Card component will be placed here
+      </div>
+    </div>
+  </div>
+  </div>
+</div>);
+}
+
+export default App;
